bdb66a5936faa8f38e85609d84cd41c2dfe7e985
SiddharthSinghRawat01 <siddharthsinghrawat01@gmail.com>
2022-04-21 17:45:35 +0530

    connect-mongo "session is stored"

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-const { cookie } = require("express/lib/response");
+const MongoStore = require('connect-mongo')(session);
+
 
 app.use(express.urlencoded());
 
@@ -37,7 +38,18 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000*60*100)
-    }
+    },
+    // mongo store is used to store the session cookie in db
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        
+        },
+        function(err){
+            console.log(err ||  'connect-mongodb setup ok');
+        }
+    )
 }));
 
 app.use(passport.initialize());
