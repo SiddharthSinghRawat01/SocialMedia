f0957bc49d5508e7d346947643d3a92a6363b9d8
SiddharthSinghRawat01 <siddharthsinghrawat01@gmail.com>
2022-05-04 12:02:11 +0530

    deleting and updating obj

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -11,6 +11,16 @@ module.exports.profile = function(req,res){
     
 }
 
+module.exports.update = function(req,res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+            return res.redirect('back');
+        });
+    }else{
+        return res.status(403).send('Unautrized');
+    }
+}
+
 // Sign UP
 module.exports.signup = function(req,res){
     if(req.isAuthenticated()){
